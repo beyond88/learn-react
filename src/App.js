90a869cbf4f3643a9458a5d7b61cacf1974864b1
beyond88 <muhin.cse.diu@gmail.com>
2023-01-29 15:20:25 +0600

    created first component(Welcome)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
+import Welcome from "./29-01-2023/Welcome";
+
 
 function App() {
   return (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
+    <Welcome  name="Johm"/>
   );
 }
 
